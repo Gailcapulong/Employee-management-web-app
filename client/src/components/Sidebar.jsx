4ab42611195a70f20ee5147cd42f00ea3835afbf
Gailcapulong <gailcapulong77@gmail.com>
2026-06-28 23:41:24 +0800

    feat: implement mobile hamburger button in Sidebar component

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { dummyProfileData } from "../assets/assets";
+import { MenuIcon } from "lucide-react";
 
 const Sidebar = () => {
   const { pathname } = useLocation();
@@ -18,7 +19,21 @@ const Sidebar = () => {
     setMobileOpen(false);
   }, [pathname]);
 
-  return <div>Sidebar</div>;
+  return (
+    <>
+      {/* mobile hamburger button */}
+
+      <button
+        onClick={() => setMobileOpen(true)}
+        className="lg:hidden fixed top-4 left-4 z-50 p-2 bg-slate-900
+      text-white rounded-lg shadow-lg border border-white/10"
+      >
+        <MenuIcon size={20} />
+      </button>
+
+      {/* mobile overlay*/}
+    </>
+  );
 };
 
 export default Sidebar;
