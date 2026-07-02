132ace5e0c788f8bbd1b7ef3767f79eb6aae83e9
Gailcapulong <gailcapulong77@gmail.com>
2026-07-02 22:10:00 +0800

    feat: add AdminDashboard component and integrate it into the Dashboard page

--- client/src/components/EmployeeDashboard.jsx
+++ client/src/components/EmployeeDashboard.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Banknote, CalendarIcon, FileTextIcon } from "lucide-react";
+import {
+  ArrowRightIcon,
+  Banknote,
+  CalendarIcon,
+  FileTextIcon,
+} from "lucide-react";
+import { Link } from "react-router-dom";
 
 const EmployeeDashboard = ({ data }) => {
   const emp = data.employee;
@@ -63,6 +69,19 @@ const EmployeeDashboard = ({ data }) => {
             />
           </div>
         ))}
+      </div>
+      <div className="flex flex-col sm:flex-row gap-3 mt-6">
+        <Link
+          to="/attendance"
+          className="btn-primary text-center inline-flex items-center
+        justify-center gap-2"
+        >
+          Mark Attendance <ArrowRightIcon className="w-4 h-4" />
+        </Link>
+
+        <Link to="/leave" className="btn-secondary text-center ">
+          Apply for Leave
+        </Link>
       </div>
     </div>
   );
